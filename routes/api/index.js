cc63f568e761a0ec9e59eb34f091c9f1beb1b78f
dopalescent <skudlgre000@gmail.com>
2023-11-06 13:08:40 -0500

    filled out routes/index and api/index

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const thoughRoutes = require('./thoughRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/thoughs', thoughRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
