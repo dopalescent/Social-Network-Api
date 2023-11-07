cad3a61da8b6d1588346817e1d2b61909f8eb5f8
dopalescent <skudlgre000@gmail.com>
2023-11-06 21:27:49 -0500

    fixed one more route typo

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const thoughtRoutes = require('./thoughtRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/thoughs', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
